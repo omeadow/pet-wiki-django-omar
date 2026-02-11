b8a4dc84ad9c64ad6be5a06800259aa384032a68
omeadow <omarmedeuov@gmail.com>
2026-02-11 09:39:32 +0500

    commit old changes

--- wiki-website/asgi.py
+++ wiki-website/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for unnamed_project project.
+ASGI config for wiki-website project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unnamed_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki-website.settings')
 
 application = get_asgi_application()
--- wiki-website/wsgi.py
+++ wiki-website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for unnamed_project project.
+WSGI config for wiki-website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unnamed_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki-website.settings')
 
 application = get_wsgi_application()
